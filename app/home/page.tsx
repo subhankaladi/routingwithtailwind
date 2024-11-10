68e185f3ed2dee703f38521d14484f6a4dbb04ab
SubhanKaladi15 <subhankaladi256.com>
2024-11-10 14:32:49 +0500

    update the css custom to tailwind

---
+++ app/home/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+
+    <header>
+      <ul className="bg-pink-500 flex justify-around py-5">
+        <li><Link href="/home" className="text-darkred no-underline">Home</Link></li>
+        <li><Link href="/about" className="text-darkred no-underline">About</Link></li>
+        <li><Link href="/contact" className="text-darkred no-underline">Contact</Link></li>
+      </ul>
+    </header>
+
+    <h1 className="text-center text-darkred font-sans">Home</h1>
+  </div>
+  )
+}
+
+export default Home;
